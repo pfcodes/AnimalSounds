b791a6e2eb7ff2976f869e24c002bff1943b6861
Phil Fevry <phil@auroradigit.al>
2020-02-21 18:20:59 -0800

    Initial Commit

---
+++ AnimalSounds/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimalSounds
+//
+//  Created by Phil on 2/21/20.
+//  Copyright © 2020 AURORA Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
